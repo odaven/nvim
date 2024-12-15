265e2c4d874d9753e0da02d2661c56eb9d06f2a9
odaven <18171281+odaven@users.noreply.github.com>
2024-12-15 21:51:57 +0000

    Changes for LazyVim v14

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,28 @@
 return {
   {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = {
+          border = "rounded",
+        },
+        documentation = {
+          window = {
+            border = "rounded",
+          },
+        },
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        lsp_doc_border = true,
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       diagnostics = {
