76b4b7dcf3f3202dfdf88c7f145207da6ab319c8
odaven <18171281+odaven@users.noreply.github.com>
2025-01-11 14:46:27 +0000

    Add border for blink's signature

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,33 +3,22 @@ return {
     "saghen/blink.cmp",
     opts = {
       completion = {
-        menu = {
-          border = "rounded",
-        },
-        documentation = {
-          window = {
-            border = "rounded",
-          },
-        },
+        menu = { border = "rounded" },
+        documentation = { window = { border = "rounded" } },
       },
+      signature = { window = { border = "rounded" } },
     },
   },
   {
     "folke/noice.nvim",
     opts = {
-      presets = {
-        lsp_doc_border = true,
-      },
+      presets = { lsp_doc_border = true },
     },
   },
   {
     "neovim/nvim-lspconfig",
     opts = {
-      diagnostics = {
-        float = {
-          border = "rounded",
-        },
-      },
+      diagnostics = { float = { border = "rounded" } },
       servers = {
         vtsls = {
           settings = {
