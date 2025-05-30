4b9bb250e6fbdae37e52eeadd0b25373a1da276e
odaven <18171281+odaven@users.noreply.github.com>
2025-05-30 14:17:50 +0100

    New winborder

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,33 +1,10 @@
 return {
   {
-    "saghen/blink.cmp",
-    opts = {
-      completion = {
-        menu = { border = "rounded" },
-        documentation = { window = { border = "rounded" } },
-      },
-      signature = { window = { border = "rounded" } },
-    },
-  },
-  {
-    "folke/noice.nvim",
-    opts = {
-      presets = { lsp_doc_border = true },
-    },
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = {
-      diagnostics = { float = { border = "rounded" } },
       servers = {
         vtsls = {
-          settings = {
-            typescript = {
-              preferences = {
-                importModuleSpecifier = "non-relative",
-              },
-            },
-          },
+          settings = { typescript = { preferences = { importModuleSpecifier = "non-relative" } } },
         },
       },
     },
