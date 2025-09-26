3083a349719fe1fb5ae085b867ec5b6e006c4809
odaven <18171281+odaven@users.noreply.github.com>
2025-09-27 00:12:24 +0100

    Dim inactive windows with autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Hide cursor line and dim windows when inactive
+vim.cmd([[
+  augroup ActiveWindow
+    autocmd!
+    " When entering a window
+    autocmd WinEnter,BufEnter * setlocal winhighlight=Normal:WinActive,CursorLine:CursorLineActive
+    autocmd WinEnter,BufEnter * setlocal cursorline
+    " When leaving a window
+    autocmd WinLeave,BufLeave * setlocal winhighlight=Normal:WinInactive,CursorLine:CursorLineInactive
+    autocmd WinLeave,BufLeave * setlocal nocursorline
+  augroup END
+
+  " Highlight groups
+  hi WinActive guibg=#000000         " active window: pure black
+  hi WinInactive guibg=#080808       " inactive windows: very dark gray (slightly lighter than black)
+  hi CursorLineActive guibg=#111111  " subtle cursorline in active window
+  hi CursorLineInactive guibg=#080808 " match inactive background
+]])
--- lua/plugins/colour-scheme.lua
+++ lua/plugins/colour-scheme.lua
@@ -6,7 +6,6 @@ return {
     priority = 1000,
     opts = {
       flavour = "mocha", -- latte, frappe, macchiato, mocha
-      dim_inactive = { enabled = true },
       color_overrides = { mocha = { base = "#000000" } },
     },
   },
@@ -16,15 +15,9 @@ return {
     priority = 1000,
     opts = {
       style = "night", -- day, moon, night, storm
-      dim_inactive = true,
       on_colors = function(colors)
         colors.bg = "#000000"
         colors.bg_float = "#000000"
-        colors.git = {
-          add = colors.green,
-          change = colors.orange,
-          delete = colors.red1,
-        }
       end,
     },
   },
@@ -33,7 +26,6 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      dimInactive = true,
       colors = { theme = { wave = { ui = { bg = "#000000" } } } },
     },
   },
