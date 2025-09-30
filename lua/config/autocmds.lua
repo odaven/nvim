59d7f3ebdce0c8953905355c22934543d5d2fa17
odaven <18171281+odaven@users.noreply.github.com>
2025-09-30 17:28:30 +0100

    Check filetypes when dimming

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,21 +2,43 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Hide cursor line and dim windows when inactive
-vim.cmd([[
-  augroup ActiveWindow
-    autocmd!
-    " When entering a window
-    autocmd WinEnter,BufEnter * setlocal winhighlight=Normal:WinActive,CursorLine:CursorLineActive
-    autocmd WinEnter,BufEnter * setlocal cursorline
-    " When leaving a window
-    autocmd WinLeave,BufLeave * setlocal winhighlight=Normal:WinInactive,CursorLine:CursorLineInactive
-    autocmd WinLeave,BufLeave * setlocal nocursorline
-  augroup END
+-- List of filetypes where you DON'T want dimming or cursorline
+local ignore_filetypes = {
+  "snacks_dashboard",
+}
 
-  " Highlight groups
-  hi WinActive guibg=#000000         " active window: pure black
-  hi WinInactive guibg=#080808       " inactive windows: very dark gray (slightly lighter than black)
-  hi CursorLineActive guibg=#111111  " subtle cursorline in active window
-  hi CursorLineInactive guibg=#080808 " match inactive background
+local function should_ignore()
+  return vim.tbl_contains(ignore_filetypes, vim.bo.filetype)
+end
+
+local group = vim.api.nvim_create_augroup("ActiveWindow", { clear = true })
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  group = group,
+  callback = function()
+    if should_ignore() then
+      return
+    end
+    vim.opt_local.winhighlight = "Normal:WinActive,CursorLine:CursorLineActive"
+    vim.opt_local.cursorline = true
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+  group = group,
+  callback = function()
+    if should_ignore() then
+      return
+    end
+    vim.opt_local.winhighlight = "Normal:WinInactive,CursorLine:CursorLineInactive"
+    vim.opt_local.cursorline = false
+  end,
+})
+
+-- Highlight groups
+vim.cmd([[
+  hi WinActive guibg=#000000
+  hi WinInactive guibg=#080808
+  hi CursorLineActive guibg=#111111
+  hi CursorLineInactive guibg=#080808
 ]])
