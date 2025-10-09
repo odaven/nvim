86746b63447afd81ca3bc186702775cc33df1f30
odaven <18171281+odaven@users.noreply.github.com>
2025-10-09 22:45:51 +0100

    Autocmds for active/inactive windows

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,39 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Show cursor line only in active window
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  callback = function()
+    if vim.w.auto_cursorline then
+      vim.wo.cursorline = true
+      vim.w.auto_cursorline = nil
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+  callback = function()
+    if vim.wo.cursorline then
+      vim.w.auto_cursorline = true
+      vim.wo.cursorline = false
+    end
+  end,
+})
+
+-- Show relative numbers in active window, absolute in inactive, only for file buffers
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  callback = function()
+    if vim.bo.buftype == "" then
+      vim.wo.relativenumber = true
+      vim.wo.number = true
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+  callback = function()
+    if vim.bo.buftype == "" then
+      vim.wo.relativenumber = false
+      vim.wo.number = true
+    end
+  end,
+})
