1efbaa61910f8d7dd2cd415ae2d8a8fed3324e22
odaven <18171281+odaven@users.noreply.github.com>
2026-01-19 16:19:16 +0000

    Lazyload themes except the main one

--- lua/plugins/colour-scheme.lua
+++ lua/plugins/colour-scheme.lua
@@ -1,6 +1,8 @@
 return {
-  { "catppuccin/nvim", lazy = false, priority = 1000 },
+  { "catppuccin/nvim", lazy = true, priority = 1000 },
   { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
-  { "rebelot/kanagawa.nvim", lazy = false, priority = 1000 },
+  { "rebelot/kanagawa.nvim", lazy = true, priority = 1000 },
+
+  -- Set the colorscheme to use
   { "LazyVim/LazyVim", opts = { colorscheme = "tokyonight-night" } },
 }
