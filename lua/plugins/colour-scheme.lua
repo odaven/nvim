e89687d91677c9239449b707ab647d6b077f7cf5
odaven <18171281+odaven@users.noreply.github.com>
2026-03-13 15:41:17 +0000

    Changes to themes

--- lua/plugins/colour-scheme.lua
+++ lua/plugins/colour-scheme.lua
@@ -1,7 +1,22 @@
 return {
-  { "catppuccin/nvim", lazy = true, priority = 1000 },
-  { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
-  { "rebelot/kanagawa.nvim", lazy = true, priority = 1000 },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    priority = 1000,
+    opts = { transparent_background = false },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = { transparent = false },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = { transparent = false },
+  },
 
   -- Set the colorscheme to use
   { "LazyVim/LazyVim", opts = { colorscheme = "tokyonight-night" } },
